8850f6d1f2275e1bad00214d98f214aa7dfe0408
Roman Mxitaryan <clasharmenia0@gmail.com>
2019-01-19 23:58:36 +0400

    verjnakan tesq

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.get('/index.html',(req,res)=>{
 	res.sendFile('index.html' , { root : __dirname});
 })
 app.get('/files/:file',(req,res)=>{
-	res.sendFile('files'+req.params.file , { root : __dirname});
+	res.sendFile('files/'+req.params.file , { root : __dirname});
 })
 app.listen(process.env.PORT || 5000)
